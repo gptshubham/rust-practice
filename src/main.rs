1ac2648a58104421d8469c2c2cd6b56c42e93e33
gptshubham <shubhamca76@gmail.com>
2025-01-23 10:18:00 +0530

    day 2/100 - explored cargo commands etc.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+/*
 fn main() {
-    println!("Hello rustc! I'm a Rust Developer.")
+    // this is a comment in Rust
+    println!("Hello rustc! I'm a Rust Developer.") // this is the key line in our program
+                                                   // println!("This line will be ignored...")
+
+    /*
+    This is a multi line comment in Rust
+    Copyright 2025
+    Last updated 23/01
+    Author: Shubham
+    */
+}
+*/
+
+fn main() {
+    println!("Hello, I'm SKG exploring Rust.");
+    println!("I'm a Front-End Developer.");
+    println!("I have built multiple vanilla JS projects.")
 }
